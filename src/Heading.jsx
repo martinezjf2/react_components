7a2a8350017153dc90623163ef7428f35ec84d4c
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-20 22:36:04 -0500

    export and import heading from heading.jsx to index.js

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -4,3 +4,5 @@ import ReactDOM from "react-dom";
 function Heading() {
   return <h1>My Favourite Foods</h1>;
 }
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
 
 ReactDOM.render(
   <div>
