0524008444ae3a997c4fbd1563e023ce936339c5
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-20 22:38:17 -0500

    created list component and export list

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import Heading from "./Heading";
 ReactDOM.render(
   <div>
     <Heading />
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
   </div>,
   document.getElementById("root")
 );
